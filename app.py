d9a2a67b20c9cbce7ac0f3c2f4c32a8656b2e4d2
LexCie10 <alexanderlawrence70@gmail.com>
2025-02-12 10:19:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ with open("model.pkl", "rb") as f:
 st.title("Vehicle One Year Resale Value")
 
 # input widget for getting user value for X (feature matrix value)
-price = st.slider("Price[thousand dollars]", min_value=0, max_value=100, value=20)
+price = st.slider("Price[thousand dollars]", color="blue", min_value=0, max_value=100, value=20)
 
 #after selecting the price, the user then submits the price value
 if st.button("Predict"):
